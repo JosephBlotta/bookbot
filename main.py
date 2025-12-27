1756df7a70ce274fb2b88e8c4d54b25f855a97a9
JosephBlotta <raphaelblotta@gmail.com>
2025-12-27 12:59:54 -0500

    Last changes to finish the chalenge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import get_num_words
+import sys
+
+from stats import char_num_count, get_num_words, sort_dic
 
 
 def get_book_text(file_path):
@@ -7,6 +9,19 @@ def get_book_text(file_path):
         return file_contents
 
 def main():
-    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
-    print(f"Found {num_words} total words")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book = get_book_text(sys.argv[1])
+        num_words = get_num_words(book)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        dic_words = sort_dic(char_num_count(book))
+        for char in dic_words:
+            print(f"{char['char']}: {char['num']}")
+        print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,24 @@
 def get_num_words(words):
     return len(words.split())
+
+def char_num_count(words):
+    char_num_dic ={}
+    chars = words.lower()
+    for char in chars:
+        if char in char_num_dic:
+            char_num_dic[char] += 1
+        else:
+            char_num_dic[char] = 1
+    return char_num_dic
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dic(dic):
+    list_of_dic = []
+    for d in dic:
+        if d.isalpha():
+            list_of_dic.append({"char":d,"num": dic[d]})
+
+    list_of_dic.sort(reverse=True, key=sort_on)
+    return list_of_dic
