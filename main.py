9e80cbe7ef2791f71834e6eb97a0daa539963179
JosephBlotta <raphaelblotta@gmail.com>
2025-12-26 20:32:32 -0500

    all done until Count Characters ex

---
+++ main.py
@@ -0,0 +1,12 @@
+from stats import get_num_words
+
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
+    print(f"Found {num_words} total words")
+main()
